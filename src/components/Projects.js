7fdd5e6e941879d0cb2599a4b3878a17459eb5d2
Terence Chambers <terencechambers91@gmail.com>
2019-07-12 20:31:10 -0400

    working on Projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-mdl';
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
   render() {
     return (
-      <div>
-        <h1>Projects</h1>
+      <div className='category-tabs'>
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>Angular</Tab>
+          <Tab>Javascript</Tab>
+          <Tab>Node</Tab>
+        </Tabs>
       </div>
     );
   }
