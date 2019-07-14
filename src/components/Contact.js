2a259e5e1c92c99144a96dc5e292c8f8652a8523
Terence Chambers <terencechambers91@gmail.com>
2019-07-14 16:29:12 -0400

    second Contact Page Commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+import { Link } from 'react-router-dom';
 
 class Contact extends Component {
   render() {
@@ -9,7 +10,7 @@ class Contact extends Component {
           <Cell col={6}>
             <h2>Terence Chambers</h2>
             <img
-              src='https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png'
+              src='https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/49680386_1715985571835995_2923186279602454528_n.jpg?_nc_cat=100&_nc_oc=AQkkXGi1Ipp4Sq8tKeiSqVwgVVXnhZeKj9Zc10wRdGAQJNcjfY1hsoiW35GLrL7wlElcbmr7pSQTKmHQDT5kOpx3&_nc_ht=scontent-ort2-1.xx&oh=ac82d53f8a53c92e61ae989032ee6554&oe=5DC2DBC3'
               alt='avatar'
               style={{ height: '250px' }}
             />
@@ -28,17 +29,42 @@ class Contact extends Component {
               <List>
                 <ListItem>
                   <ListItemContent
-                    style={{ fontSize: '25px', fontFamily: 'Anton' }}
+                    style={{
+                      fontSize: '20px',
+                      fontFamily: 'Anton',
+                      fontWeight: 'bold'
+                    }}
                   >
-                    <i className='fa fa-phone-square' aria-hidden='true' />
-                    000-000-0000
+                    <i class='fa fa-envelope-o' aria-hidden='true' />
+                    <Link to='www.terencechambers91@gmail.com' />
+                    www.terencechambers91@gmail.com
                   </ListItemContent>
                 </ListItem>
                 <ListItem>
-                  <ListItemContent icon='person'>Aaron Paul</ListItemContent>
+                  <ListItemContent
+                    style={{
+                      fontSize: '21px',
+                      fontFamily: 'Anton',
+                      fontWeight: 'bold'
+                    }}
+                  >
+                    <i class='fa fa-linkedin' aria-hidden='true' />
+                    <Link to='https://www.linkedin.com/in/terence-chambers-59666787/'>
+                      /Terence Chambers
+                    </Link>
+                  </ListItemContent>
                 </ListItem>
                 <ListItem>
-                  <ListItemContent icon='person'>Bob Odenkirk</ListItemContent>
+                  <ListItemContent
+                    style={{
+                      fontSize: '20px',
+                      fontFamily: 'Anton',
+                      fontWeight: 'bold'
+                    }}
+                  >
+                    <i class='fa fa-skype' aria-hidden='true' />
+                    tknile@aol.com
+                  </ListItemContent>
                 </ListItem>
               </List>
             </div>
