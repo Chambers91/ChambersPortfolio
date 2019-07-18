4c352da54552598afe7b47226191e5f29f460aa7
Terence Chambers <terencechambers91@gmail.com>
2019-07-18 19:29:49 -0400

    About Page- added image and text

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Aboutme extends Component {
   render() {
     return (
-      <div>
-        <h1>About Me</h1>
+      <div style={{ width: '100%', margin: 'auto', alignItems: 'center' }}>
+        <Grid className='landing-grid'>
+          <Cell col={12}>
+            <img
+              src='https://avatars3.githubusercontent.com/u/26388609?s=460&v=4'
+              alt='Chambers_Avatar'
+              className='avatar'
+            />
+            <div className='banner-text'>
+              <hr />
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              </p>
+
+            </div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
