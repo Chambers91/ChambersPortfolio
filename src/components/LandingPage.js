ec8001befe9637c474bd46626069702587b64255
Terence Chambers <terencechambers91@gmail.com>
2019-09-09 12:10:03 -0400

    web refactor on full stack textbox

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -11,47 +11,44 @@ class LandingPage extends Component {
       <div style={{ width: '100%', margin: 'auto', alignItems: 'center' }}>
         <Grid className='landing-grid'>
           <Cell col={12}>
-            <img
-              src='https://media.licdn.com/dms/image/C4E03AQEApoV6z0FEkg/profile-displayphoto-shrink_200_200/0?e=1567641600&v=beta&t=g4cMaYJziPPjsaWdUxEscvZAXgKq0uq4G46MsdBklE4'
-              alt='Chambers_Avatar'
-              className='avatar'
-            />
+            <img src='Headshot.jpeg' alt='Chambers_Avatar' className='avatar' />
             <div className='banner-text'>
-              <h1 style={{ fontFamily: 'Righteous', fontSize: '8vw' }}>
+              <h1 style={{ fontFamily: 'Righteous', fontSize: '6vw' }}>
                 Full Stack Web Developer
               </h1>
               <hr />
-              <p style={{ fontFamily: 'Righteous' }}>
-                HTML | CSS | Bootstrap | Angular | React | NodeJS | Express |
-                MongoDB
-              </p>
-              <div className='social-links'>
-                {/* LinkedIn*/}
-                <a
-                  href='https://www.linkedin.com/in/chambers91/'
-                  rel='noopener noreferrer'
-                  target='_blank'
-                >
-                  <i className='fa fa-linkedin-square' aria-hidden='true' />
-                </a>
-                {/* GitHub*/}
-                <a
-                  href='https://github.com/Chambers91'
-                  rel='noopener noreferrer'
-                  target='_blank'
-                >
-                  <i class='fa fa-github-square  ' aria-hidden='true' />
-                </a>
-                {/* Resume*/}
-                <a
-                  href='https://drive.google.com/file/d/135pNZUAicnfQDznfJqRHS1l_zSZN5-ze/view?usp=sharing'
-                  rel='noopener noreferrer'
-                  target='_blank'
-                  download='Chambers_Resume'
-                >
-                  <i class='fa fa-download' aria-hidden='true' />
-                </a>
-              </div>
+            </div>
+
+            <p style={{ fontFamily: 'Righteous' }}>
+              HTML | CSS | Bootstrap | Angular | React | NodeJS | Express |
+              MongoDB
+            </p>
+            <div className='social-links'>
+              {/* LinkedIn*/}
+              <a
+                href='https://www.linkedin.com/in/chambers91/'
+                rel='noopener noreferrer'
+                target='_blank'
+              >
+                <i className='fa fa-linkedin-square' aria-hidden='true' />
+              </a>
+              {/* GitHub*/}
+              <a
+                href='https://github.com/Chambers91'
+                rel='noopener noreferrer'
+                target='_blank'
+              >
+                <i class='fa fa-github-square  ' aria-hidden='true' />
+              </a>
+              {/* Resume*/}
+              <a
+                href='https://drive.google.com/file/d/135pNZUAicnfQDznfJqRHS1l_zSZN5-ze/view?usp=sharing'
+                rel='noopener noreferrer'
+                target='_blank'
+                download='Chambers_Resume'
+              >
+                <i class='fa fa-download' aria-hidden='true' />
+              </a>
             </div>
             <hr />
             <h1 style={{ fontFamily: 'Righteous' }}>About Me</h1>
